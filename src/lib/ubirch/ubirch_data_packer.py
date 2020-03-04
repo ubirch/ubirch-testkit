85346301ccd41d1616e3304d5beb611fa4c796f2
Roxana Meixner <roxana.meixner@gmail.com>
2020-03-04 11:56:28 +0100

    better docstring

--- src/lib/ubirch/ubirch_data_packer.py
+++ src/lib/ubirch/ubirch_data_packer.py
@@ -9,11 +9,12 @@ MSG_TYPE = 1
 
 def pack_data_msgpack(uuid: UUID, data: dict) -> (bytes, bytes):
     """
-    Generate a message for the ubirch data service.
+    Generate a msgpack formatted message for the ubirch data service.
+    The message contains the device UUID, timestamp and data to ensure unique hash.
+    The sha512 hash of the message will be appended to the message.
     :param uuid: the device UUID
-    :param data: a map containing the data to be sent
-    :return: a msgpack formatted array with the device UUID, message type, timestamp, data and hash
-    :return: the hash of the data message
+    :param data: the mapped data to be sent to the ubirch data service
+    :return: the msgpack formatted message, the hash of the data message
     """
     msg = [
         uuid.bytes,
