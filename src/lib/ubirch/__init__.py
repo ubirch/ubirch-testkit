296a1314752ae47b3473f3eebd9f49987f13cf96
Roxana Meixner <roxana.meixner@gmail.com>
2020-03-08 16:19:05 +0100

    updated init for better encapsulation

--- src/lib/ubirch/__init__.py
+++ src/lib/ubirch/__init__.py
@@ -1,4 +1,3 @@
-from .ubirch_api import *
-from .ubirch_client import *
-from .ubirch_ks import *
-from .ubirch_protocol import *
+from ubirch_client import UbirchClient
+from ubirch_api import API
+from ubirch_helpers import *
