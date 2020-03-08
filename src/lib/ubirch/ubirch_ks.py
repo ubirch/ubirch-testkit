7513d70e5a25e2463e1ece117d4f58c08ec1e5c6
Roxana Meixner <roxana.meixner@gmail.com>
2020-03-08 11:25:52 +0100

    name -> uuid map

--- src/lib/ubirch/ubirch_ks.py
+++ src/lib/ubirch/ubirch_ks.py
@@ -1,7 +1,5 @@
 import os
-
 import ed25519
-
 from uuid import UUID
 
 
@@ -10,29 +8,34 @@ class KeyStore:
     The ubirch key store handles the keys relevant for the ubirch protocol.
     """
 
-    def __init__(self, uuid: UUID, cfg_root: str = "") -> None:
-        self.uuid = uuid
-        self._ks_file = str(uuid) + ".bin"
+    def __init__(self, cfg_root: str = "") -> None:
         self._cfg_root = cfg_root
+        self._names = {}
+        self._sks = {}
         self._vks = {}
-        self._load_keys()
 
-    def _load_keys(self) -> None:
-        """Load or create new crypto-keys. The keys are stored in a local key store."""
-        if self._ks_file in os.listdir(self._cfg_root):
-            print("** loading existing key pair for " + str(self.uuid))
-            with open(self._cfg_root + self._ks_file, "rb") as kf:
-                self._sk = ed25519.SigningKey(kf.read())
-                self._vk = self._sk.get_verifying_key()
+    def load_keys(self, name: str, uuid: UUID) -> None:
+        """Load or create new crypto-keys. The keys are stored in a local key file."""
+        ks_file = str(uuid) + ".bin"
+        if ks_file in os.listdir(self._cfg_root):
+            print("** loading existing key pair for " + str(uuid))
+            with open(self._cfg_root + ks_file, "rb") as kf:
+                sk = ed25519.SigningKey(kf.read())
+                self.insert_keypair(name, uuid, sk)
         else:
-            print("** generating new key pair for " + str(self.uuid))
-            (self._vk, self._sk) = ed25519.create_keypair()
-            with open(self._cfg_root + self._ks_file, "wb") as kf:
-                kf.write(self._sk.to_bytes())
-        self.insert_verifying_key(self.uuid, self._vk)
+            print("** generating new key pair for " + str(uuid))
+            (vk, sk) = ed25519.create_keypair()
+            self.insert_keypair(name, uuid, sk)
+            with open(self._cfg_root + ks_file, "wb") as kf:
+                kf.write(sk.to_bytes())
 
-    def insert_verifying_key(self, uuid: UUID, verifying_key: ed25519.VerifyingKey):
+    def insert_verifying_key(self, name: str, uuid: UUID, verifying_key: ed25519.VerifyingKey):
         self._vks[uuid.hex] = verifying_key
+        self._names[name] = uuid.hex
+
+    def insert_keypair(self, name: str, uuid: UUID, signing_key: ed25519.SigningKey):
+        self._sks[uuid.hex] = signing_key
+        self.insert_verifying_key(name, uuid, signing_key.get_verifying_key())
 
     def get_verifying_key(self, uuid: UUID) -> ed25519.VerifyingKey:
         try:
@@ -40,21 +43,36 @@ class KeyStore:
         except KeyError:
             raise Exception("No known verifying key for UUID {}".format(uuid))
 
-    def get_signing_key(self) -> ed25519.SigningKey:
-        return self._sk
+    def get_signing_key(self, uuid: UUID) -> ed25519.SigningKey:
+        try:
+            return self._sks[uuid.hex]
+        except KeyError:
+            raise Exception("No known signing key for UUID {}".format(uuid))
 
-    def get_certificate(self) -> dict:
+    def get_verifying_key_with_name(self, name: str) -> ed25519.VerifyingKey:
+        try:
+            return self.get_verifying_key(self._names[name])
+        except KeyError:
+            raise Exception("No known verifying key for name {}".format(name))
+
+    def get_signing_key_with_name(self, name: str) -> ed25519.SigningKey:
+        try:
+            return self.get_signing_key(self._names[name])
+        except KeyError:
+            raise Exception("No known signing key for name {}".format(name))
+
+    def get_certificate(self, uuid: UUID) -> dict:
         """Get a self signed certificate for the public key"""
 
-        pubkey = self._vk.to_bytes()
-        created = os.stat(self._cfg_root + self._ks_file)[7]
+        pubkey = self.get_verifying_key(uuid).to_bytes()
+        created = os.stat(self._cfg_root + str(uuid) + ".bin")[7]
         not_before = created
         # TODO fix handling of key validity
         not_after = created + 30758400
         return {
             "algorithm": 'ECC_ED25519',
             "created": created,
-            "hwDeviceId": self.uuid.bytes,
+            "hwDeviceId": uuid.bytes,
             "pubKey": pubkey,
             "pubKeyId": pubkey,
             "validNotAfter": not_after,
