ae320e9d4c67ca5073a7c2ccb7b45b25f0d820a1
Roxana Meixner <roxana.meixner@gmail.com>
2020-03-09 09:46:58 +0100

    minor fix

--- src/lib/ubirch/__init__.py
+++ src/lib/ubirch/__init__.py
@@ -1,3 +1,3 @@
-from ubirch_client import UbirchClient
-from ubirch_api import API
-from ubirch_helpers import *
+from .ubirch_client import UbirchClient
+from .ubirch_api import API
+from .ubirch_helpers import *
