d7ad0f63cad98f82b25db9fe035f61555046301d
Roxana Meixner <roxana.meixner@gmail.com>
2020-06-11 19:26:28 +0200

    clean up

--- src/lib/ubirch/ubirch_client.py
+++ src/lib/ubirch/ubirch_client.py
@@ -11,17 +11,6 @@ from .ubirch_sim import SimProtocol
 class UbirchClient:
 
     def __init__(self, cfg: dict, lte: LTE, imsi: str):
-        vks = {
-            "dev": a2b_base64(
-                "LnU8BkvGcZQPy5gWVUL+PHA0DP9dU61H8DBO8hZvTyI7lXIlG1/oruVMT7gS2nlZDK9QG+ugkRt/zTrdLrAYDA=="),
-            "demo": a2b_base64(
-                "LnU8BkvGcZQPy5gWVUL+PHA0DP9dU61H8DBO8hZvTyI7lXIlG1/oruVMT7gS2nlZDK9QG+ugkRt/zTrdLrAYDA==")
-        }
-        uuids = {
-            "dev": UUID(unhexlify("9d3c78ff22f34441a5d185c636d486ff")),
-            "demo": UUID(unhexlify("9d3c78ff22f34441a5d185c636d486ff"))
-        }
-
         self.key_name = "ukey"
         self.api = API(cfg)
         self.sim = SimProtocol(lte=lte, at_debug=cfg['debug'])
@@ -31,20 +20,12 @@ class UbirchClient:
         if not self.sim.sim_auth(pin):
             raise Exception("PIN not accepted")
 
-        # store public key of ubirch backend on the SIM card
-        env = self.api.env
-        if not self.sim.entry_exists(env):
-            self.sim.store_public_key(env, uuids[env], vks[env])
-
         # get UUID from SIM
         self.uuid = self.sim.get_uuid(self.key_name)
         print("** UUID   : " + str(self.uuid) + "\n")
 
         # send a X.509 Certificate Signing Request for the public key to the ubirch identity service
         submit_csr(self.uuid, self.key_name, self.sim, self.api)
-
-        # # register public key at ubirch key service todo will be replaced by X.509 cert
-        # register_public_key(self.uuid, self.key_name, self.sim, self.api)
 
     def send(self, data: dict):
         """
@@ -67,23 +48,8 @@ class UbirchClient:
 
         # send UPP to the ubirch authentication service to be anchored to the blockchain
         print("** sending UPP ...\n")
-        response = self.api.send_upp(self.uuid, upp)
-        print("response: " + hexlify(response).decode())
-        # verify the signature of the backend response with its public key
-        try:
-            fixed_upp = fix_upp_signature_format(response)
-            print("fixed response: " + hexlify(fixed_upp).decode())
-            if not self.sim.message_verify(self.api.env, fixed_upp):
-                raise Exception("signature verification failed")
-        except Exception as e:
-            raise Exception("!! couldn't verify backend response: {} ({}) ".format(e, hexlify(response).decode()))
+        self.api.send_upp(self.uuid, upp)
 
         # retrieve data message hash from generated UPP for verification
         message_hash = get_upp_payload(upp)
         print("** data message hash: {}".format(b2a_base64(message_hash).decode()))
-
-        # # OPTIONAL # verify that the hash was received and verifiable by the backend
-        # print("** verifying hash in backend (quick check) ...")
-        # time.sleep(0.2)  # wait for the backend to be ready
-        # response = self.api.verify(message_hash, quick=True)
-        # print("** verification successful! response: " + response.decode())
--- src/lib/ubirch/ubirch_helpers.py
+++ src/lib/ubirch/ubirch_helpers.py
@@ -1,5 +1,3 @@
-import asn1
-import machine
 import os
 import time
 import ubinascii as binascii
@@ -9,51 +7,10 @@ from .ubirch_api import API
 from .ubirch_sim import SimProtocol
 
 
-def asn1tosig(data: bytes):
-    s1 = asn1.asn1_node_root(data)
-    a1 = asn1.asn1_node_first_child(data, s1)
-    part1 = asn1.asn1_get_value(data, a1)
-    a2 = asn1.asn1_node_next(data, a1)
-    part2 = asn1.asn1_get_value(data, a2)
-    if len(part1) > 32: part1 = part1[1:]
-    if len(part2) > 32: part2 = part2[1:]
-    return part1 + part2
-
-
-def register_public_key(uuid: UUID, key_name: str, sim: SimProtocol, api: API):
-    """
-    Register a public key at the ubirch key service
-    """
-    cert_file = "cert_{}_{}.bin".format(str(uuid), api.env)
-    if cert_file not in os.listdir():
-        print("** registering public key at key service ...")
-        cert = create_certificate(uuid, key_name, sim)
-        api.register_key(cert)
-        with open(cert_file, "wb") as f:
-            f.write(cert)
-
-
-def create_certificate(uuid: UUID, key_name: str, sim: SimProtocol) -> bytes:
-    """
-    Get a self-signed JSON formatted public key certificate
-    """
-    TIME_FMT = '{:04d}-{:02d}-{:02d}T{:02d}:{:02d}:{:02d}.000Z'
-    now = machine.RTC().now()
-    created = not_before = TIME_FMT.format(now[0], now[1], now[2], now[3], now[4], now[5])
-    later = time.localtime(time.mktime(now) + 30758400)
-    not_after = TIME_FMT.format(later[0], later[1], later[2], later[3], later[4], later[5])
-    pub_base64 = binascii.b2a_base64(sim.get_key(key_name)).decode()[:-1]
-    # json must be compact and keys must be sorted alphabetically
-    REG_TMPL = '{{"algorithm":"ecdsa-p256v1","created":"{}","hwDeviceId":"{}","pubKey":"{}","pubKeyId":"{}","validNotAfter":"{}","validNotBefore":"{}"}}'
-    REG = REG_TMPL.format(created, str(uuid), pub_base64, pub_base64, not_after, not_before).encode()
-    # get the ASN.1 encoded signature and extract the signature bytes from it
-    signature = asn1tosig(sim.sign(key_name, REG, 0x00))
-    return '{{"pubKeyInfo":{},"signature":"{}"}}'.format(REG.decode(),
-                                                         binascii.b2a_base64(signature).decode()[:-1]).encode()
-
-
 def bootstrap(imsi: str, api: API) -> str:
-    # load PIN or bootstrap if PIN unknown
+    """
+    Load PIN or bootstrap if PIN unknown
+    """
     pin_file = imsi + ".bin"
     pin = ""
     if pin_file in os.listdir():
