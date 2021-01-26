445edcf9df0e53095c5f961ff78f2ce5b1128f33
Roxana Meixner <roxana.meixner@ubirch.com>
2021-01-26 09:48:52 +0100

    use actual backend ecdsa public keys

--- src/lib/ubirch/ubirch_sim.py
+++ src/lib/ubirch/ubirch_sim.py
@@ -432,7 +432,7 @@ class SimProtocol:
 
     def store_backend_public_keys(self):
         """
-        Store the ECC UBIRCH backend public keys in the SIM cards secure storage
+        Store the UBIRCH backend public keys in the SIM cards secure storage
         Throws exception if operation fails.
         """
         backend_keys = {
@@ -442,15 +442,14 @@ class SimProtocol:
                     "LnU8BkvGcZQPy5gWVUL+PHA0DP9dU61H8DBO8hZvTyI7lXIlG1/oruVMT7gS2nlZDK9QG+ugkRt/zTrdLrAYDA==")
             },
             "demo": {
-                "uuid": UUID(unhexlify("9d3c78ff22f34441a5d185c636d486ff")),
+                "uuid": UUID(unhexlify("0710423518924020904200003c94b60b")),
                 "pubkey": a2b_base64(
-                    "LnU8BkvGcZQPy5gWVUL+PHA0DP9dU61H8DBO8hZvTyI7lXIlG1/oruVMT7gS2nlZDK9QG+ugkRt/zTrdLrAYDA==")
+                    "xm+iIomBRjR3QdvLJrGE1OBs3bAf8EI49FfgBriRk36n4RUYX+0smrYK8tZkl6Lhrt9lzjiUGrXGijRoVE+UjA==")
             },
             "prod": {
-                "uuid": UUID(unhexlify("9d3c78ff22f34441a5d185c636d486ff")),  # fixme use actual backend UUID
+                "uuid": UUID(unhexlify("10b2e1a456b34fff9adacc8c20f93016")),
                 "pubkey": a2b_base64(
-                    "LnU8BkvGcZQPy5gWVUL+PHA0DP9dU61H8DBO8hZvTyI7lXIlG1/oruVMT7gS2nlZDK9QG+ugkRt/zTrdLrAYDA==")
-                # fixme use actual backend ecdsa public key
+                    "pJdYoJN0N3QTFMBVjZVQie1hhgumQVTy2kX9I7kXjSyoIl40EOa9MX24SBAABBV7xV2IFi1KWMnC1aLOIvOQjQ==")
             },
         }
 
